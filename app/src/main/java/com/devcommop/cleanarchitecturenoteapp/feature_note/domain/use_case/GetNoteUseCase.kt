a7c4269acc0044438689d29bcd70e63d4078446d
Joaquin144 <vvpp898.comco@gmail.com>
2022-08-07 15:56:20 +0530

    GetNoteUseCase (singular)

---
+++ app/src/main/java/com/devcommop/cleanarchitecturenoteapp/feature_note/domain/use_case/GetNoteUseCase.kt
@@ -0,0 +1,14 @@
+package com.devcommop.cleanarchitecturenoteapp.feature_note.domain.use_case
+
+import com.devcommop.cleanarchitecturenoteapp.feature_note.domain.model.Note
+import com.devcommop.cleanarchitecturenoteapp.feature_note.domain.repository.NoteRepository
+
+class GetNoteUseCase(
+    private val repository: NoteRepository
+) {
+
+    suspend operator fun invoke(id: Int): Note? {
+        return repository.getNoteById(id = id)
+    }
+
+}
