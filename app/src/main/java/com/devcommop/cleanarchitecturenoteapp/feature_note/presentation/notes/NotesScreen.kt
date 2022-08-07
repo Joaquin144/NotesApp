c0881924693a44d874cfcddaa852b22cb3812124
Joaquin144 <vvpp898.comco@gmail.com>
2022-08-07 15:28:35 +0530

    NotesScreen UI done

---
+++ app/src/main/java/com/devcommop/cleanarchitecturenoteapp/feature_note/presentation/notes/NotesScreen.kt
@@ -0,0 +1,116 @@
+package com.devcommop.cleanarchitecturenoteapp.feature_note.presentation.notes
+
+import androidx.compose.animation.*
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Sort
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import com.devcommop.cleanarchitecturenoteapp.feature_note.presentation.notes.components.NoteItem
+import com.devcommop.cleanarchitecturenoteapp.feature_note.presentation.notes.components.OrderSection
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalAnimationApi::class)
+@Composable
+fun NotesScreen(
+    navController: NavController,
+    viewModel: NotesViewModel = hiltViewModel()
+) {
+    val state = viewModel.state.value
+    val scaffoldState = rememberScaffoldState()//to show snackbars etc. we maintain a Scaffold state
+    val scope = rememberCoroutineScope()//begin to for snackbar
+
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                    //todo: Navigate to add note screen
+                },
+                backgroundColor = MaterialTheme.colors.primary
+            ) {
+                Icon(Icons.Default.Add, contentDescription = "Add Note")
+            }
+        },
+        scaffoldState = scaffoldState
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "Your Notes",
+                    style = MaterialTheme.typography.h4
+                )
+                IconButton(
+                    onClick = {
+                        viewModel.onEvent(NotesEvent.ToggleOrderSection)//we won't manually change the state of VM that's a bad practice. Rather we'll send an event to VM which it would infer and change the state variable
+                    }
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Sort,
+                        contentDescription = "Hide/Show Order Section Button"
+                    )
+                }
+                AnimatedVisibility(
+                    visible = state.isOrderSectionVisible,
+                    enter = fadeIn() + slideInVertically(),//fadeIN and slideInVertically both are combined. Compose OP _/\_
+                    exit = fadeOut() + slideOutVertically()
+                ) {
+                    OrderSection(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 16.dp),
+                            noteOrder = state.noteOrder
+                        ,
+                        onOrderChange = { viewModel.onEvent(NotesEvent.Order(it)) }
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            LazyColumn(modifier = Modifier.fillMaxSize()){
+                items(state.notes) { note ->
+                    NoteItem(
+                        note = note,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clickable {
+                                //todo: navigate to add/edit note screen
+                            },
+                        onDeleteClick = {
+                            viewModel.onEvent(NotesEvent.DeleteNote(note))
+                            //Now show the snackbar for certain amount of time:-------
+                            scope.launch {
+                                val result = scaffoldState.snackbarHostState.showSnackbar(
+                                    message = "Note deleted",
+                                    actionLabel = "Undo"
+                                )
+                                //Now apply the undo action
+                                if(result == SnackbarResult.ActionPerformed){
+                                    viewModel.onEvent(NotesEvent.RestoreNote)
+                                }
+                            }
+                        }
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                }
+            }
+        }
+    }
+
+}
